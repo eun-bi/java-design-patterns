dea08bd7e7d5f101f099cfc399ff942150c9f285
eun-bi <uko02111@naver.com>
2019-01-31 14:31:36 +0900

    Add: Factory Method 패턴 README 파일 추가

--- Factory-Method/src/Main.java
+++ Factory-Method/src/Main.java
@@ -16,13 +16,12 @@ public class Main {
         
 		// 제품 생산
 		Product card1 = factory.create("홍길동");
-        Product card2 = factory.create("이순신");
-        Product card3 = factory.create("강감찬");
-        
-        // 제품 사용
-        card1.use();
-        card2.use();
-        card3.use();
+		Product card2 = factory.create("이순신");
+		Product card3 = factory.create("강감찬");
+		
+		// 제품 사용
+		card1.use();
+		card2.use();
+		card3.use();
 	}
-
 }
